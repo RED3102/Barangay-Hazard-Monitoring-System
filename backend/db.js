91bc8acb43466677a9a1a4f9922dbec80511c4a4
Red Chua <red.chua22@gmail.com>
2026-05-01 16:24:14 +0800

    fix aiven ssl using env variable

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql2/promise');
-const fs    = require('fs');
 require('dotenv').config();
 
 const pool = mysql.createPool({
@@ -9,7 +8,8 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    ca: fs.readFileSync('./ca.pem'),
+    ca:                 process.env.DB_SSL_CA,
+    rejectUnauthorized: true
   },
   waitForConnections: true,
   connectionLimit:    10,
